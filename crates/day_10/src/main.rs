76cf3fe53a0864723b5164d2cc64d9e50d2561ed
Jonathan Samson <jonathansamsonvcu@gmail.com>
2025-07-16 17:12:28 -0400

    better setup functions

---
+++ crates/day_10/src/main.rs
@@ -0,0 +1,14 @@
+use advent_of_code_2024::aoc;
+
+fn main() {
+    let input = include_str!("input.txt");
+    aoc::run_parts(input, part_1, part_2);
+}
+
+fn part_1(input: &str) -> i32 {
+    0
+}
+
+fn part_2(input: &str) -> i32 {
+    0
+}
