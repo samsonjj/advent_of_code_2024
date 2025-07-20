0fcf6b158933cda54d2cfded6faec74a055e422f
Jonathan Samson <jonathansamsonvcu@gmail.com>
2025-07-20 13:20:58 -0400

    day_15 part 1

--- crates/day_15/src/main.rs
+++ crates/day_15/src/main.rs
@@ -35,45 +35,31 @@ impl Game {
     }
     fn run_sim(&mut self) {
         for i in 0..self.moves.len() {
-            self.move_once(self.moves[i]);
-        }
-    }
-
-    fn move_once(&mut self, direction: (i32, i32)) {
-        let (dy, dx) = direction;
-        let next_pos = (self.robot.0 + dy, self.robot.1 + dx);
-        let next_spot = self.map[next_pos.0 as usize][next_pos.1 as usize];
-        
-        if next_spot == Spot::Empty {
-            self.robot = next_pos;
-        }
-
-        if next_spot == Spot::Wall {
-            return;
-        }
-
-        if next_spot == Spot::Box {
-            // attempt to push box
-            if self.push_box(next_pos, direction) {
-                self.robot = next_pos;
-            }
+            self.push(self.robot, self.moves[i]);
         }
     }
 
     // returns true if pushing the box was successful
-    fn push_box(&mut self, pos: (i32, i32), direction: (i32, i32)) -> bool {
+    fn push(&mut self, pos: (i32, i32), direction: (i32, i32)) -> bool {
         let (dy, dx) = direction;
         let next_pos = (pos.0 + dy, pos.1 + dx);
         let next_spot = self.map[next_pos.0 as usize][next_pos.1 as usize];
         match next_spot {
             Spot::Empty => {
+                if pos == self.robot {
+                    self.robot = next_pos;
+                }
                 self.map[next_pos.0 as usize][next_pos.1 as usize] = Spot::Box;
                 self.map[pos.0 as usize][pos.1 as usize] = Spot::Empty;
                 true
             }
             Spot::Box => {
-                if self.push_box(next_pos, direction) {
-                    self.map[next_pos.0 as usize][next_pos.1 as usize] = Spot::Box;
+                if self.push(next_pos, direction) {
+                    if pos == self.robot {
+                        self.robot = next_pos;
+                    }
+                    self.map[next_pos.0 as usize][next_pos.1 as usize]
+                        = self.map[pos.0 as usize][pos.1 as usize];
                     self.map[pos.0 as usize][pos.1 as usize] = Spot::Empty;
                     true
                 } else {
@@ -84,6 +70,19 @@ impl Game {
                 false
             }
         }
+    }
+
+    fn score(&self) -> i32 {
+        let mut score = 0;
+        for row in 0..self.map.len() {
+            for col in 0..self.map[row].len() {
+                if self.map[row][col] != Spot::Box || self.robot == (row as i32, col as i32) {
+                    continue;
+                }
+                score += (row * 100 + col) as i32;
+            }
+        }
+        score
     }
 }
 
@@ -120,7 +119,7 @@ fn parse_input(input: &str) -> Game {
 }
 
 fn main() {
-    let input = include_str!("example.txt");
+    let input = include_str!("input.txt");
     aoc::run_parts(input, part_1, part_2);
 }
 
@@ -128,7 +127,7 @@ fn part_1(input: &str) -> i32 {
     let mut game = parse_input(input);
     game.run_sim();
     game.print();
-    0
+    game.score()
 }
 
 fn part_2(input: &str) -> i32 {
