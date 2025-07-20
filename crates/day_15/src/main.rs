ccb9927faacea5da986ac9baeb6725fdc4309198
Jonathan Samson <jonathansamsonvcu@gmail.com>
2025-07-20 14:15:44 -0400

    day_15

--- crates/day_15/src/main.rs
+++ crates/day_15/src/main.rs
@@ -5,6 +5,8 @@ use itertools::Itertools;
 enum Spot {
     Wall,
     Box,
+    LBox,
+    RBox,
     Empty,
 }
 
@@ -26,6 +28,8 @@ impl Game {
                 print!("{}", match self.map[row][col] {
                     Spot::Empty => '.',
                     Spot::Box=> 'O',
+                    Spot::LBox=> '[',
+                    Spot::RBox=> ']',
                     Spot::Wall => '#',
                 });
             }
@@ -34,52 +38,91 @@ impl Game {
         println!();
     }
     fn run_sim(&mut self) {
+        // self.print();
         for i in 0..self.moves.len() {
-            self.push(self.robot, self.moves[i]);
+            if self.push(self.robot, self.moves[i], false) {
+                self.push(self.robot, self.moves[i], true);
+            }
+            // println!("Move: {}", match self.moves[i] {
+            //     (1, 0) => 'v',
+            //     (-1, 0) => '^',
+            //     (0, 1) => '>',
+            //     (0, -1) => '<',
+            //     _ => unreachable!(),
+            // });
+            // self.print();
         }
     }
 
+    fn widen(mut self) -> Self {
+        self.map = self.map.into_iter().map(|row| {
+            row.into_iter().map(|spot| {
+                match spot {
+                    Spot::Empty => vec![Spot::Empty, Spot::Empty],
+                    Spot::Wall => vec![Spot::Wall, Spot::Wall],
+                    Spot::Box => vec![Spot::LBox, Spot::RBox],
+                    _ => unreachable!(),
+                }
+            })
+        }).map(|row| row.flatten().collect_vec()).collect_vec();
+        self.robot = (self.robot.0, self.robot.1 * 2);
+        self
+    }
+
+    fn get(&self, pos: (i32, i32)) -> Spot {
+        self.map[pos.0 as usize][pos.1 as usize]
+    }
+
     // returns true if pushing the box was successful
-    fn push(&mut self, pos: (i32, i32), direction: (i32, i32)) -> bool {
+    fn push(&mut self, pos: (i32, i32), direction: (i32, i32), execute: bool) -> bool {
         let (dy, dx) = direction;
         let next_pos = (pos.0 + dy, pos.1 + dx);
         let next_spot = self.map[next_pos.0 as usize][next_pos.1 as usize];
-        match next_spot {
-            Spot::Empty => {
-                if pos == self.robot {
-                    self.robot = next_pos;
-                }
-                self.map[next_pos.0 as usize][next_pos.1 as usize] = Spot::Box;
-                self.map[pos.0 as usize][pos.1 as usize] = Spot::Empty;
-                true
-            }
-            Spot::Box => {
-                if self.push(next_pos, direction) {
-                    if pos == self.robot {
-                        self.robot = next_pos;
-                    }
-                    self.map[next_pos.0 as usize][next_pos.1 as usize]
-                        = self.map[pos.0 as usize][pos.1 as usize];
-                    self.map[pos.0 as usize][pos.1 as usize] = Spot::Empty;
-                    true
+        let can_move = match next_spot {
+            Spot::Empty => true,
+            Spot::Box => self.push(next_pos, direction, execute),
+            Spot::LBox =>
+                if direction.0 == 0 {
+                // horizontal
+                    self.push(next_pos, direction, execute)
                 } else {
-                    false
+                // vertical
+                    let adjacent = if self.get(next_pos) == Spot::LBox { (next_pos.0, next_pos.1 + 1) } else { (next_pos.0, next_pos.1 - 1) };
+                    self.push(next_pos, direction, execute)
+                        && self.push(adjacent, direction, execute)
                 }
+            Spot::RBox =>
+                if direction.0 == 0 {
+                // horizontal
+                    self.push(next_pos, direction, execute)
+                } else {
+                // vertical
+                    let adjacent = if self.get(next_pos) == Spot::LBox { (next_pos.0, next_pos.1 + 1) } else { (next_pos.0, next_pos.1 - 1) };
+                    // println!("hello, ({}, {})|({}, {})", pos.0, pos.1, adjacent.0, adjacent.1);
+                    self.push(next_pos, direction, execute)
+                        && self.push(adjacent, direction, execute)
+                }
+            Spot::Wall => false,
+        };
+        // println!("({}, {})", pos.0, pos.1);
+        if can_move && execute {
+            if pos == self.robot {
+                self.robot = next_pos;
             }
-            Spot::Wall => {
-                false
-            }
+            self.map[next_pos.0 as usize][next_pos.1 as usize]
+                = self.map[pos.0 as usize][pos.1 as usize];
+            self.map[pos.0 as usize][pos.1 as usize] = Spot::Empty;
         }
+        can_move
     }
 
     fn score(&self) -> i32 {
         let mut score = 0;
         for row in 0..self.map.len() {
             for col in 0..self.map[row].len() {
-                if self.map[row][col] != Spot::Box || self.robot == (row as i32, col as i32) {
-                    continue;
+                if self.map[row][col] == Spot::Box || self.map[row][col] == Spot::LBox {
+                    score += (row * 100 + col) as i32;
                 }
-                score += (row * 100 + col) as i32;
             }
         }
         score
@@ -126,10 +169,14 @@ fn main() {
 fn part_1(input: &str) -> i32 {
     let mut game = parse_input(input);
     game.run_sim();
-    game.print();
+    // game.print();
     game.score()
 }
 
 fn part_2(input: &str) -> i32 {
-    0
+    let mut game = parse_input(input);
+    game = game.widen();
+    game.run_sim();
+    // game.print();
+    game.score()
 }
